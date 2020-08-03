9d0818341967638920b2e8751d85ebc77cfe7860
HXgrowns <804156017@qq.com>
2020-08-03 16:21:47 +0800

    [huxiao]{add}: add calculate_hit_not_10_score test.

---
+++ src/main/java/Bowling.java
@@ -0,0 +1,5 @@
+public class Bowling {
+    public static int calculate(int firstHit, int secondHit) {
+        return firstHit + secondHit;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BowlingGameTest {
+    @Test
+    void calculate_hit_not_10_score() {
+        int firstHit = 3;
+        int secondHit = 3;
+        int expected = Bowling.calculate(firstHit, secondHit);
+        assertEquals(6,expected);
+    }
+
 }
